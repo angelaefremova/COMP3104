84fa91713948195b067331730c4548a59e61a169
angelaefremova <35670694+angelaefremova@users.noreply.github.com>
2022-10-04 14:30:16 -0400

    React code project added

---
+++ build/precache-manifest.a5a7500d3b9b17c34856debd69ff5995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7359e00a2d0c20d7aa8",
+    "url": "/angelaefremova/comp3104/static/css/main.f827ec55.chunk.css"
+  },
+  {
+    "revision": "e7359e00a2d0c20d7aa8",
+    "url": "/angelaefremova/comp3104/static/js/main.e7359e00.chunk.js"
+  },
+  {
+    "revision": "594a241fcdbbab0f28fd",
+    "url": "/angelaefremova/comp3104/static/js/runtime~main.594a241f.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/angelaefremova/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "c9acec8c00058d83b7837d60846fb987",
+    "url": "/angelaefremova/comp3104/index.html"
+  }
+];
